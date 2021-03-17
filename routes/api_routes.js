b3a7f7a82820ee6503d2812c28e9cc8beb6f5a3b
BrienB <barrdom@hotmail.com>
2021-03-17 00:13:24 -0400

    renamed api.js to api_routes.js

---
+++ routes/api_routes.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ day: -1 })
+    .limit(1)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+// router.get("/exercise/?:id", (req, res) => {
+//   Workout.findOne(
+//     {
+//     _id: mongojs.ObjectId(req.params.id)
+//     }
+//   )
+//   .then(dbWorkout => {
+//     res.json(dbWorkout);
+//   })
+//   .catch(err => {
+//     res.status(400).json(err);
+//   });
+// });
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findOneAndUpdate(
+    {
+      _id: mongojs.ObjectId(req.params.id)
+    },
+    {
+      $set: { body }
+    }
+  )
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+// router.post("/api/workouts", ({ body }, res) => {
+//   Workout.create(body)
+//     .then(dbWorkout => {
+//       res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -17,6 +17,7 @@ mongoose.connect("mongodb://localhost/workout", {
 });
 
 app.use(require("./routes/api.js"));
+app.use(require("./routes/html_routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
