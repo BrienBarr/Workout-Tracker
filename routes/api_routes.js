74829ff0780e994bb637cd349fd35e147e25d495
BrienB <barrdom@hotmail.com>
2021-03-17 03:25:07 -0400

    updated get exercise route to find document using req.query.id

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -13,43 +13,60 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-// router.get("/exercise/?:id", (req, res) => {
-//   Workout.findOne(
-//     {
-//     _id: req.params.id
-//     }
-//   )
-//   .then(dbWorkout => {
-//     res.json(dbWorkout);
-//   })
-//   .catch(err => {
-//     res.status(400).json(err);
-//   });
-// });
-
-router.put("/api/workouts/:id", (req, res) => {
-  Workout.update(
-    {
-      _id: req.params.id
-    },
-    req.body
-  )
-  .then(dbWorkout => {
-    res.json(dbWorkout);
-  })
-  .catch(err => {
-    res.status(400).json(err);
-  });
-});
-
-router.post("/api/workouts", ({ body }, res) => {
-  Workout.create({body})
+router.get("/exercise", (req, res) => {
+  console.log(req.query.id);
+  if(req.query.id){
+    Workout.findOne(
+      {
+      _id: req.query.id
+      }
+    )
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
+  } else {
+    res.redirect("./exercise.html");
+  }
 });
+
+router.put("/api/workouts/:id", (req, res) => {
+  // console.log(body);
+  if (req.params.id === "undefined"){
+    createWorkout(req.body, res);
+  } else{
+    Workout.updateOne(
+      {
+        _id: req.params.id
+      },
+      {exercises: {$push: {body}}}
+    )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+  }
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  // console.log(body);
+  createWorkout(body, res);
+});
+
+function createWorkout(data, res){
+  console.log(data);
+  Workout.create(data)
+  .then(dbWorkout => {
+    console.log(dbWorkout);
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+}
 
 module.exports = router;
