2281bcd6441fdf25fb1ff34d89ae5ebbb73558c6
BrienB <barrdom@hotmail.com>
2021-03-17 03:23:47 -0400

    removed commented out code

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
