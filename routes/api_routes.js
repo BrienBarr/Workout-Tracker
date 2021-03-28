1a222e5e7f533d2d10143e19eb5070faf5d514e9
BrienB <barrdom@hotmail.com>
2021-03-28 12:51:01 -0400

    updated api/workouts/:id put route to add exercises to current workout

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-// const Workout = require("../models/workout.js");
 const db = require("../models");
 
 router.get("/api/workouts", (req, res) => {
@@ -16,15 +15,29 @@ router.get("/api/workouts", (req, res) => {
   });
 });
 
-let exercises = [];
-
 router.put("/api/workouts/:id", (req, res) => {
-  console.log(req.params.id);
-  console.log("Request body: " + JSON.stringify(req.body));
+  db.Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } }, 
+    { new: true }
+  )
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
 });
 
 router.post("/api/workouts", (req, res) => {
-  createWorkout(req.body, res);
+  db.Workout.create({})
+  .then(dbWorkout => {
+    console.log(dbWorkout);
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
 });
 
 router.get("/api/workouts/range", (req, res) => {
@@ -40,17 +53,5 @@ router.get("/api/workouts/range", (req, res) => {
     res.status(400).json(err);
   });
 })
-
-function createWorkout(data, res){
-  console.log("data: " + JSON.stringify(data));
-  db.Workout.create({})
-  .then(dbWorkout => {
-    console.log(dbWorkout);
-    res.json(dbWorkout);
-  })
-  .catch(err => {
-    res.status(400).json(err);
-  });
-}
 
 module.exports = router;
