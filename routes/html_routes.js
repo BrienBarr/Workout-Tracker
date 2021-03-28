87f94c6a947b2c2f553b20b34bd9d5f0662a0f9c
BrienB <barrdom@hotmail.com>
2021-03-28 12:51:33 -0400

    cleaned up code

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 router.get("/exercise", function(req, res){
   res.sendFile(path.join(__dirname, '../public', 'exercise.html'));
+  // res.redirect("./exercise.html");
 });
 
 router.get("/stats", function(req, res){
